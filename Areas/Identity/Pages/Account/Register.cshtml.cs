1331c9ca6919cdfc04f867a8c31fb5e66ce45a83
DotaJack <jackphorgan12@gmail.com>
2024-01-24 23:54:19 +0000

    Email as Username

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -100,6 +100,10 @@ namespace HonestAuto.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
 
             [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
             [Display(Name = "Role")]
             public string SelectedRole { get; set; }
         }
@@ -118,7 +122,7 @@ namespace HonestAuto.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
+                user.UserName = Input.UserName;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
